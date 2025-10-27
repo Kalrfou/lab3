a2c7f5e4fc0d5cabbd550e99af0f6f7488b61f03
Kalrfou <47829362+Kalrfou@users.noreply.github.com>
2025-10-27 13:14:16 -0600

    update by adding new files

---
+++ script.js
@@ -0,0 +1,132 @@
+// JavaScript DOM Lab - 40 Mini Exercises
+
+// 🟩 PART 1: Selecting and Styling Elements (1–10)
+
+// 1. Change text color of an element by ID to red
+// TODO:
+
+// 2. Change background color of all <p> to lightgray
+// TODO:
+
+// 3. Change #title text to "Welcome to the DOM Lab!"
+// TODO:
+
+// 4. Add solid border to all <div>
+// TODO:
+
+// 5. Change font size of first <h1> to 36px
+// TODO:
+
+// 6. Change text color of elements with class 'highlight' to blue
+// TODO:
+
+// 7. Add 'active' class to an element when page loads
+// TODO:
+
+// 8. Change <body> background color to lightblue
+// TODO:
+
+// 9. Set font-family of all <p> to Arial
+// TODO:
+
+// 10. Change image source of #sampleImage
+// TODO:
+
+
+// 🟦 PART 2: Creating and Appending Elements (11–20)
+
+// 11. Create a new <p> and append to <body>
+// TODO:
+
+// 12. Create new <li> and append to existing <ul>
+// TODO:
+
+// 13. Create a <button> labeled "Click Me!" inside a <div>
+// TODO:
+
+// 14. Create <h2> dynamically and insert before first <p>
+// TODO:
+
+// 15. Create an image and set width/height
+// TODO:
+
+// 16. Add 3 new <li> items to list using a loop
+// TODO:
+
+// 17. Create a <div> with class "card" and add text
+// TODO:
+
+// 18. Append paragraph with your name to document
+// TODO:
+
+// 19. Add <span>[Edited]</span> inside every <p>
+// TODO:
+
+// 20. Remove last child of <ul>
+// TODO:
+
+
+// 🟨 PART 3: Event Handling (21–30)
+
+// 21. Add click event to button that alerts “Button clicked!”
+// TODO:
+
+// 22. Change background color of <div> when clicked
+// TODO:
+
+// 23. On hover over <p>, make text bold
+// TODO:
+
+// 24. On mouse leave, remove bold style
+// TODO:
+
+// 25. Add button that hides image when clicked
+// TODO:
+
+// 26. Add button that shows hidden image again
+// TODO:
+
+// 27. Counter button: increment number on each click
+// TODO:
+
+// 28. Input + button: display entered text in <p>
+// TODO:
+
+// 29. Change background color randomly when button clicked
+// TODO:
+
+// 30. Display pressed key in <p>
+// TODO:
+
+
+// 🟧 PART 4: Modifying and Traversing DOM (31–40)
+
+// 31. Change text of first <li> to "First item updated!"
+// TODO:
+
+// 32. Get input value on button click
+// TODO:
+
+// 33. Count number of <p> and alert the count
+// TODO:
+
+// 34. Remove all <p> from the page
+// TODO:
+
+// 35. Replace existing <h2> with new one
+// TODO:
+
+// 36. Add class 'highlighted' to all <li>
+// TODO:
+
+// 37. Toggle class on <div> when clicked
+// TODO:
+
+// 38. Clone an element and append copy to page
+// TODO:
+
+// 39. Scroll smoothly to bottom of page
+// TODO:
+
+// 40. Use innerHTML to build list from array
+// TODO:
